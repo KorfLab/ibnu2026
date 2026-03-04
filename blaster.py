df77ce6144fd63d996efc44b9285894a47c92d7d
Ian Korf <ifkorf@gmail.com>
2026-03-04 13:19:55 -0800

    up

--- blaster.py
+++ blaster.py
@@ -4,11 +4,11 @@ import os
 import sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('mRNA_file', help='.fa.gz')
-parser.add_argument('reads_dir', help='fastq files')
+parser.add_argument('mRNA_file', help='*.fa.gz')
+parser.add_argument('reads_dir', help='*fastq.gz')
 parser.add_argument('--build', default='build',
 	help='build directory [%(default)s]')
-parser.add_argument('--cpus', type=int, default=8, help='[%(default)i]')
+parser.add_argument('--cpus', type=int, default=4, help='[%(default)i]')
 parser.add_argument('--testing', action='store_true')
 arg = parser.parse_args()
 
@@ -23,7 +23,7 @@ params = ' '.join((
 	f'-i {arg.build}/temp.fa',  # query file is temporary
 	f'-a {arg.cpus}',           # depends on computer
 	'-r 1 -q -1',               # scoring system: +1, -1
-	'-G 2 -E 1',                # gap system: -2 to open, -1 to exten
+	'-G 2 -E 1',                # gap system: -2 to open, -1 to extend
 	'-e 1e-10',                 # filter off poor matches
 	'-m 8')                     # use tabular output
 )
