9d686341938fb7a26fd9a71080d2ab15a4b67114
iankorf <ifkorf@gmail.com>
2026-03-06 07:29:18 -0800

    up

--- README.md
+++ README.md
@@ -4,18 +4,27 @@ ibnu2026
 ## Aligner Test ##
 
 Each aligner runs in its own environment, which is in the `envs` directory.
+Install them all as follows:
 
-Aligners (at the time of testing on macOS-arm64)
+```
+cd envs
+ls | xargs -I {} conda env create -f {}
+```
 
-- bbmap 39.77
-- blast-legacy 2.2.26
-- bowtie2 2.5.5
-- bwa 0.7.19
-- gmap 2025.07.31
-- hisat2 2.2.2
-- minimap2 2.30
-- pblat 2.5.1
-- segemehl 0.3.4
-- star 2.7.11b
-- subread 2.1.1
+At the time of testing, all of the aligners have the same version number in
+both macOS-arm64 and linux-64 except bbmap.
+
+| Program      | macOS-arm64 | linux-64
+|:-------------|:------------|:-----------
+| bbmap        | 39.77       | 39.79
+| blast-legacy | 2.2.26      | 2.2.26
+| bowtie2      | 2.5.5       | 2.5.5
+| bwa          | 0.7.19      | 0.7.19
+| gmap         | 2025.07.31  | 2025.07.31
+| hisat2       | 2.2.2       | 2.2.2
+| minimap2     | 2.30        | 2.30
+| pblat        | 2.5.1       | 2.5.1
+| segemehl     | 0.3.4       | 0.3.4
+| star         | 2.7.11b     | 2.7.11b
+| subread      | 2.1.1       | 2.1.1
 
--- contest.py
+++ contest.py
@@ -1,17 +1,35 @@
 import argparse
 import os
+import platform
 import random
 import re
 import sys
 
 import korflab
 
+def run(cli):
+	pid = os.getpid()
+	tempfile = f'{DIR}/{pid}.temp'
+	print(cli, file=sys.stderr)
+	if os.system(f'{TIMER} {cli} 2> {tempfile}') != 0: sys.exit(f'FAILED: {cli}')
+	runstats = {}
+	with open(tempfile) as fp:
+		for line in fp:
+			if re.match(r'\s+User', line):
+				runstats['utime'] = float(line.split()[-1])
+			elif re.match(r'\s+System', line):
+				runstats['stime'] = float(line.split()[-1])
+			elif re.match(r'\s+Maximum', line):
+				runstats['rsize'] = int(line.split()[-1])
+	os.remove(tempfile)
+	return runstats
+
 def simulate_reads(infile, outfile, rlen, xcov, rate):
 	rate /= 100
 	with open(outfile, 'w') as fp:
 		for defline, seq in korflab.readfasta(infile):
 			chrom = defline.split()[0]
-			rnum = int(len(seq) * xcov / rlen)			
+			rnum = int(len(seq) * xcov / rlen)
 			for i in range(rnum):
 				pos = random.randint(0, len(seq) - rlen)
 				if 'NN' in seq[pos:pos+rlen]: continue
@@ -35,20 +53,20 @@ def simulate_reads(infile, outfile, rlen, xcov, rate):
 					rdef += '+'
 				print(rdef, ''.join(rseq), sep='\n', file=fp)
 
-def test_bbmap(gfile, rfile):
-	pass
+def test_bbmap(gfile, rfile, cpus):
+	run(f'/usr/bin/time -v conda run -n bbmap bbmap.sh in={rfile} ref={gfile} nodisk=t threads={cpus} out={rfile}.bbmap > {rfile}.stdout 2> {rfile}.stderr')
 
 
 def test_blast(gfile, rfile, cpus):
-	os.system(f'conda run -n blast-legacy formatdb -p F -i {gfile}')
-	os.system(f'conda run -n blast-legacy blastall -p blastn -d {gfile} -i {rfile} -a {cpus} -r 1 -q -1 -G 2 -E 1 -e 1e-10 -m 8 > {rfile}.blastn')
+	r1 = run(f'conda run -n blast-legacy formatdb -p F -i {gfile}')
+	r2 = run(f'conda run -n blast-legacy blastall -p blastn -d {gfile} -i {rfile} -a {cpus} -r 1 -q -1 -G 2 -E 1 -e 1e-10 -m 8 -o {rfile}.blastn')
 	seen = set()
 	aligned = 0
 	total = 0
 	with open(f'{rfile}.blastn') as fp:
 		for line in fp:
 			f = line.split()
-			m = re.search('^(\w+):(\d+)\-(\d+)(.)$', f[0])
+			m = re.search(r'^(\w+):(\d+)\-(\d+)(.)$', f[0])
 			if m:
 				c1 = m.group(1)
 				b1 = int(m.group(2))
@@ -66,26 +84,28 @@ def test_blast(gfile, rfile, cpus):
 			seen.add( (c1, b1, e1, s1) )
 			aligned += alen
 			total += tlen
-	return aligned / total
+	cpu = r1['utime'] + r1['stime'] + r2['utime'] + r2['stime']
+	mem = max(r1['rsize'], r2['rsize'])
+	return aligned / total, cpu, mem
 
 
-def test_bowtie2(gfile, rfile):
+def test_bowtie2(gfile, rfile, cpus):
 	pass
-def test_bwa(gfile, rfile):
+def test_bwa(gfile, rfile, cpus):
 	pass
-def test_gmap(gfile, rfile):
+def test_gmap(gfile, rfile, cpus):
 	pass
-def test_hisat2(gfile, rfile):
+def test_hisat2(gfile, rfile, cpus):
 	pass
-def test_minimap2(gfile, rfile):
+def test_minimap2(gfile, rfile, cpus):
 	pass
-def test_pblat(gfile, rfile):
+def test_pblat(gfile, rfile, cpus):
 	pass
-def test_segemehl(gfile, rfile):
+def test_segemehl(gfile, rfile, cpus):
 	pass
-def test_star(gfile, rfile):
+def test_star(gfile, rfile, cpus):
 	pass
-def test_subread(gfile, rfile):
+def test_subread(gfile, rfile, cpus):
 	pass
 
 parser = argparse.ArgumentParser(description='compare alignment programs')
@@ -100,26 +120,31 @@ parser.add_argument('--seed', type=int, default=1)
 parser.add_argument('--testing', action='store_true')
 arg = parser.parse_args()
 
-## set up build directory
+## set up GLOBALS
 DIR = f'{arg.build}/contest'
 if not os.path.exists(f'{DIR}/genome.fa'):
 	os.system(f'mkdir -p {DIR}')
 	os.system(f'gunzip -c {arg.genome} > {DIR}/genome.fa')
 
+match platform.system():
+	case 'Linux':  TIMER = '/usr/bin/time -v'
+	case 'Darwin': TIMER = '/usr/bin/time -l'
+	case _: sys.exit('error, OS not supported')
+
 ## main loop
 random.seed(arg.seed)
-for err_rate in range(35):
+for err_rate in (1, 10):
 	gfile = f'{DIR}/genome.fa'
 	rfile = f'{DIR}/reads.{arg.seed}.{err_rate}.fa'
 	simulate_reads(gfile, rfile, arg.rlen, arg.x, err_rate)
-	test_bbmap(gfile, rfile)
+	#test_bbmap(gfile, rfile, arg.cpus)
 	print(err_rate, 'blast', test_blast(gfile, rfile, arg.cpus), sep='\t')
-	test_bowtie2(gfile, rfile)
-	test_bwa(gfile, rfile)
-	test_gmap(gfile, rfile)
-	test_hisat2(gfile, rfile)
-	test_minimap2(gfile, rfile)
-	test_pblat(gfile, rfile)
-	test_segemehl(gfile, rfile)
-	test_star(gfile, rfile)
-	test_subread(gfile, rfile)
+	#test_bowtie2(gfile, rfile, arg.cpus)
+	#test_bwa(gfile, rfile, arg.cpus)
+	#test_gmap(gfile, rfile, arg.cpus)
+	#test_hisat2(gfile, rfile, arg.cpus)
+	#test_minimap2(gfile, rfile, arg.cpus)
+	#test_pblat(gfile, rfile, arg.cpus)
+	#test_segemehl(gfile, rfile, arg.cpus)
+	#test_star(gfile, rfile, arg.cpus)
+	#test_subread(gfile, rfile, arg.cpus)
